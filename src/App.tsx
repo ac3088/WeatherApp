b3ffab5e9a602108694712feac8d29f3f2fb9a09
ac3088 <achewlun@gmail.com>
2024-02-05 18:44:23 +1100

    cleaned up how the app looks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import Box from '@mui/material/Box';
 import Container from '@mui/system/Container';
 import Grid from '@mui/system/Unstable_Grid';
 import Stack from '@mui/joy/Stack';
 
 import TextField from '@mui/material/TextField';
-
 import IconButton from '@mui/material/IconButton';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import Switch from '@mui/material/Switch';
 
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 import WbTwilightIcon from '@mui/icons-material/WbTwilight';
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import CloudIcon from '@mui/icons-material/Cloud';
@@ -18,28 +19,50 @@ import './App.css'
 
 const API_KEY: string = '25034e1902ed4c92981114031241501';
 
+const boxStyles = {
+  borderColor: 'black',
+  border: 1,
+  borderRadius: '16px',
+  height: '12rem',
+  width: '12rem',
+  padding: '1rem',
+  margin: 'auto',
+};
+
 function App() {
 
   function updateTemperature(city: string) {
     const apiURL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+
+    const tempDisplayContainer: any = document.getElementById('temp-display');
+    const extraInfoContainer: any = document.getElementById('extra-info-display');
+
     const temperatureP: any = document.getElementById('temperature-p');
-    const localtimeP: any = document.getElementById('localtime-p');
-    const uvP: any = document.getElementById('uv-p');
-    const humidityP: any = document.getElementById('humidity-p');
+    const conditionP: any = document.getElementById('condition-p');
+
     const precipP: any = document.getElementById('precip-p');
     const windP: any = document.getElementById('wind-p');
+    const humidityP: any = document.getElementById('humidity-p');
+    const feelsLikeP: any = document.getElementById('feelslike-p');
+    const uvP: any = document.getElementById('uv-p');
     const visP: any = document.getElementById('vis-p');
 
     fetch(apiURL)
       .then(response => response.json())
       .then(data => {
         temperatureP.innerHTML = `${data.current.temp_c}°C`;
-        localtimeP.innerHTML = `${data.location.localtime}`;
-        uvP.innerHTML = `${data.current.uv}`;
-        humidityP.innerHTML = `${data.current.humidity}%`;
-        precipP.innerHTML = `${data.current.precip_mm}mm`;
-        windP.innerHTML = `${data.current.wind_kph}kph ${data.current.wind_dir}`;
-        visP.innerHTML = `${data.current.vis_km}km`;
+        conditionP.innerHTML = `${data.current.condition.text}`;
+
+        precipP.innerHTML = `Precipitation<br />${data.current.precip_mm}mm`;
+        windP.innerHTML = `Wind<br />${data.current.wind_kph}kph ${data.current.wind_dir}`;
+        humidityP.innerHTML = `Humidity<br />${data.current.humidity}%`;
+        feelsLikeP.innerHTML = `Feels Like<br />${data.current.feelslike_c}°C`;
+        uvP.innerHTML = `UV Index<br />${data.current.uv}`;
+        visP.innerHTML = `Visibility<br />${data.current.vis_km}km`;
+
+        tempDisplayContainer.style.visibility = "visible";
+        extraInfoContainer.style.visibility = "visible";
+
         console.log(data);
       });
   }
@@ -52,39 +75,57 @@ function App() {
 
   return (
     <>
-      <Stack>
+      <Stack id="main-stack">
         <Container id="search-bar">
           <TextField id="location-input" label="Location" variant="outlined" />
           <IconButton onClick={() => search()}><TravelExploreIcon fontSize="large" /></IconButton>
+          {/* TODO: Implement switch for changing between Celcius and Fahrenheit (and miles and km) */}
         </Container>
+
         <Container id="temp-display">
-          <p id='temperature-p'></p>
+          <Stack>
+            <p id='temperature-p'>0°C</p>
+            <p id='condition-p'>Clear</p>
+          </Stack>
         </Container>
-        <Container id="climate-display">
-          <Grid container spacing={2}>
+
+        <Container id="extra-info-display">
+          <Grid container rowSpacing={8} sx={{ width: '75%', margin: 'auto' }}>
             <Grid xs={4}>
-              <AccessTimeIcon fontSize="large" />
-              <p id='localtime-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <CloudIcon fontSize="large" />
+                <p id='precip-p'>Precipitation<br />0mm</p>
+              </Box>
             </Grid>
             <Grid xs={4}>
-              <WbTwilightIcon fontSize="large" />
-              <p id='uv-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <AirIcon fontSize="large" />
+                <p id='wind-p'>Wind<br />0kph N</p>
+              </Box>
             </Grid>
             <Grid xs={4}>
-              <WaterDropIcon fontSize="large" />
-              <p id='humidity-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <WaterDropIcon fontSize="large" />
+                <p id='humidity-p'>Humidity<br />0%</p>
+              </Box>
             </Grid>
             <Grid xs={4}>
-              <CloudIcon fontSize="large" />
-              <p id='precip-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <ThermostatIcon fontSize="large" />
+                <p id='feelslike-p'>Feels Like<br />0°C</p>
+              </Box>
             </Grid>
             <Grid xs={4}>
-              <AirIcon fontSize="large" />
-              <p id='wind-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <WbTwilightIcon fontSize="large" />
+                <p id='uv-p'>UV Index<br />0</p>
+              </Box>
             </Grid>
             <Grid xs={4}>
-              <VisibilityIcon fontSize="large" />
-              <p id='vis-p'></p>
+              <Box sx={{ ...boxStyles }}>
+                <VisibilityIcon fontSize="large" />
+                <p id='vis-p'>Visibility<br />0km</p>
+              </Box>
             </Grid>
           </Grid>
         </Container>
